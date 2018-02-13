723a4128adb7ea6a1c9f2540e31bd1ac76419fdf
BerTepesh <Ber.Tepesh@gmail.com>
2018-02-13 12:03:48 +0300

    Переплет

--- f/js/common.js
+++ f/js/common.js
@@ -1000,10 +1000,6 @@ function onloadRecaptcha() {
 }
 
 $(document).ready(function() {
-  /*$(".scroll-down__btn").click(function(){
-    $(this).closest(".scroll-down__item").toggleClass("active");
-    return false;
-  });*/
 	inteoJSCore.init();
 	/*
 	$(".fancybox").fancybox({
